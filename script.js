8f83469619083f078778e0001368aa17e1289564
B-MNSN <manutsanan0@gmail.com>
2022-05-17 16:37:57 +0700

    fibo

--- script.js
+++ script.js
@@ -1,16 +1,24 @@
-let n = 3
-let fac_for = 1
-for (let i = 1; i <= n; i++) {
-    fac_for = fac_for * i
+let n = 10;
+let fibo_for = 1;
+let fn1 = 0;
+let fn2 = 1;
+for (let i = 2; i < n; i++) {
+    fn2 = fn1;
+    fn1 = fibo_for;
+    fibo_for = fn2 + fn1;
 }
-console.log(fac_for)
+console.log(fibo_for);
 
-let fac_while = 1
-let count = 1
-while (count <= n) {
-    fac_while = fac_while * count
-    count++
+let fibo_while = 1;
+let count = 2;
+fn1 = 0;
+fn2 = 1;
+while (count < n) {
+    fn2 = fn1;
+    fn1 = fibo_while;
+    fibo_while = fn2 + fn1;
+    count++;
 }
-console.log(fac_while)
+console.log(fibo_while);
 
-console.log(fac_for === fac_while)
+console.log(fibo_for === fibo_while)
