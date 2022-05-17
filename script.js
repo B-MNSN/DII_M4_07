a823ea264602804e7b67b0901c34d0ca572582b4
B-MNSN <manutsanan0@gmail.com>
2022-05-17 14:16:48 +0700

    fac

--- script.js
+++ script.js
@@ -1,20 +1,16 @@
-let money = 999999
-let tax
-if (money <= 150000) {
-    tax = 0
-} else if (money <= 300000) {
-    tax = (money - 150000) * (5 / 100)
-} else if (money <= 500000) {
-    tax = ((money - 300000) * (10 / 100)) + 7500
-} else if (money <= 750000) {
-    tax = ((money - 500000) * (15 / 100)) + 27500
-} else if (money <= 1000000) {
-    tax = ((money - 750000) * (20 / 100)) + 65000
-} else if (money <= 2000000) {
-    tax = ((money - 1000000) * (25 / 100)) + 115000
-} else if (money <= 5000000) {
-    tax = ((money - 2000000) * (30 / 100)) + 365000
-} else if (money > 5000000) {
-    tax = ((money - 5000000) * (35 / 100)) + 1265000
+let n = 3
+let fac_for = 1
+for (let i = 1; i <= n; i++) {
+    fac_for = fac_for * i
 }
-console.log('You have to pay' + ' ' + tax)
+console.log(fac_for)
+
+let fac_while = 1
+let count = 1
+while (count <= n) {
+    fac_while = fac_while * count
+    count++
+}
+console.log(fac_while)
+
+console.log(fac_for === fac_while)
