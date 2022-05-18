857986adaf23d09ade3d99a75a2490e25293ace1
B-MNSN <manutsanan0@gmail.com>
2022-05-18 12:08:46 +0700

    not ok

--- script.js
+++ script.js
@@ -1,20 +1,2 @@
-let score = 0
-let grade
-if (score >= 80) {
-    grade = 'A'
-} else if (score >= 75) {
-    grade = 'B+'
-} else if (score >= 70) {
-    grade = 'B'
-} else if (score >= 65) {
-    grade = 'c+'
-} else if (score >= 60) {
-    grade = 'c'
-} else if (score >= 55) {
-    grade = 'D+'
-} else if (score >= 50) {
-    grade = 'D'
-} else if (score < 50) {
-    grade = 'F'
-}
-console.log(grade + ' ' + score)
+const newText = 'not ok';
+document.getElementById('ok').innerHTML = newText;
