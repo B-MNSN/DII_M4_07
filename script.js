481a6b920e95a9d2c118301d08a890d12955426b
B-MNSN <manutsanan0@gmail.com>
2022-05-18 11:00:32 +0700

    place

--- script.js
+++ script.js
@@ -1,16 +1,17 @@
-let cmu = {
+let place = [3]
+place[0] = {
     'name': 'chaing mai university',
     'lat': 18.8046735,
     'long': 98.9528446,
 }
-let maya = {
+place[1] = {
     'name': 'maya',
     'lat': 18.8047037,
     'long': 98.9440898,
 }
-let cenfest = {
+place[2] = {
     'name': 'central chaing mai',
     'lat': 18.8048018,
     'long': 98.917825,
 }
-console.log(cmu.name + ' ' + maya.name)
+console.log(place)
