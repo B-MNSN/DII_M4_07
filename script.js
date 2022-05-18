ced7eb0c6151204e800725bfceaab85f43bf98d7
B-MNSN <manutsanan0@gmail.com>
2022-05-18 12:37:04 +0700

    date

--- script.js
+++ script.js
@@ -2,9 +2,16 @@ function alert1() {
     alert('let me see ...');
 }
 let ok = document.getElementById('ok');
-// ok.addEventListener('click', alert1)
 ok.addEventListener('click', function() {
     alert1('let me see ...');
 });
 let cancel = document.getElementById('cancel');
-cancel.addEventListener('click', () => alert('let me see ...'));
+cancel.addEventListener('click', () => alert('not ok ...'));
+
+let showDate = document.getElementById('showDate');
+showDate.addEventListener('click', function(e) {
+    document.getElementById('emptySpace').innerHTML = "<b>" + Date() + "</b>";
+});
+showDate.addEventListener('dblclick', function(e) {
+    document.getElementById('emptySpace').innerHTML = '';
+});
