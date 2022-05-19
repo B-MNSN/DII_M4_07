b3911c7c69e770519db4e5423e9acde8dfbcb5ea
B-MNSN <manutsanan0@gmail.com>
2022-05-19 17:21:40 +0700

    update-tax

--- script.js
+++ script.js
@@ -31,18 +31,44 @@ employee[4] = {
 }
 console.log(employee)
 
+let taxAvg = 0
+for (let i = 0; i < employee.length; i++) {
+    taxAvg += findTax(employee[i].salary * 12)
+}
+
+function findTax(money) {
+    let tax = 0
+    if (money <= 150000) {
+        tax = 0
+    } else if (money <= 300000) {
+        tax = (money - 150000) * (5 / 100)
+    } else if (money <= 500000) {
+        tax = ((money - 300000) * (10 / 100)) + 7500
+    } else if (money <= 750000) {
+        tax = ((money - 500000) * (15 / 100)) + 27500
+    } else if (money <= 1000000) {
+        tax = ((money - 750000) * (20 / 100)) + 65000
+    } else if (money <= 2000000) {
+        tax = ((money - 1000000) * (25 / 100)) + 115000
+    } else if (money <= 5000000) {
+        tax = ((money - 2000000) * (30 / 100)) + 365000
+    } else if (money > 5000000) {
+        tax = ((money - 5000000) * (35 / 100)) + 1265000
+    }
+    tax = tax / employee.length
+    return tax
+}
+
 let salarySum = 0
 for (let i = 0; i < employee.length; i++) {
     salarySum += employee[i].salary;
 }
-console.log(salarySum);
 
 let ageAvg = 0;
 for (let i = 0; i < employee.length; i++) {
     ageAvg += employee[i].age;
 }
 ageAvg = ageAvg / employee.length;
-console.log(ageAvg);
 
 function findSum(array, key) {
     sum = 0;
@@ -61,5 +87,6 @@ function findAvg(array, key) {
 salarySum = findSum(employee, 'salary');
 ageAvg = findAvg(employee, 'age');
 
-console.log(salarySum);
-console.log(ageAvg);
+console.log('Salary Avg : ' + salarySum);
+console.log('Age Avg : ' + ageAvg);
+console.log('Tax Avg : ' + taxAvg)
